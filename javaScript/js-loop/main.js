04b30065f00bbf23c91bcbd28938ff3b4700903d
kishorekumar <kishoremech2698@gmail.com>
2023-11-30 07:15:03 +0530

    while loop

--- javaScript/js-loop/main.js
+++ javaScript/js-loop/main.js
@@ -4,6 +4,7 @@ const players = [
   { name: "Dhoni", favNum: 7, favColour: "yellow" },
 ];
 
+
 // for loop
 
 // for (let i = 0; i < players.length; i++) {
@@ -19,12 +20,33 @@ const players = [
 // for in
 
 // for (let i in players) {
-//   let sort = players[i].name;
-//   console.log(sort);
+//   let playerName = players[i].name;
+//   console.log(playerName);
 // }
 
 // for of
 
-for (let i of players) {
-  console.log(i.name + " your favourite colour is " + i.favColour);
-}
+// for (let i of players) {
+//   console.log(i.name + " your favourite colour is " + i.favColour);
+// }
+
+// while loop
+// players.sort((a,b)=>{
+//   if (a.favNum < b.favNum) return -1;
+//   else if (a.favNum > b.favNum) return 1;
+//   else return 0;
+// });
+
+// let i = 0;
+// while (i < players.length) {
+//   console.log(players[i].favNum);
+//   i++;
+// }
+
+// do while 
+// let i = 0;
+// do {
+//   console.log(players[i].favColour);
+//   i++;
+// } while (i< players.length);
+
