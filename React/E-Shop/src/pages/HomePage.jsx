b279b0d72f42a8f2371077a7f9988831ee717ec1
kishorekumar <kishoremech2698@gmail.com>
2024-02-20 09:07:18 +0530

    fix json

--- React/E-Shop/src/pages/HomePage.jsx
+++ React/E-Shop/src/pages/HomePage.jsx
@@ -2,17 +2,18 @@ import ItemsCard from "../components/ItemsCard";
 import products from "../assets/products.json";
 
 const HomePage = () => {
-
-
-  return products.map((product, index) => {
-    const { brand, type, price, img } = product;
-    return (
-      <div className="" key={index}>
-        <ItemsCard brand={brand} type={type} price={price} img={img}/>
-      </div>
-
-    );
-  });
+  return (
+    <div className="grid grid-cols-4">
+      {products.map((product, index) => {
+        const { brand, type, price, img } = product;
+        return (
+          <div  key={index}>
+            <ItemsCard brand={brand} type={type} price={price} img={img} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default HomePage;
